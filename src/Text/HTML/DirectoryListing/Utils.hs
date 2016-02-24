3014ae8b06e70569ddb608cf378cf0159b84a702
mingchuan <ming@culpring.com>
2016-02-24 20:24:57 +0800

    add two comparing function

--- src/Text/HTML/DirectoryListing/Utils.hs
+++ src/Text/HTML/DirectoryListing/Utils.hs
@@ -3,11 +3,16 @@ where
 
 import Network.URI (unEscapeString)
 import Type
+import Data.Ord
 import qualified Data.Text as T
 
 decodedName :: Entry -> T.Text
 decodedName = T.pack . unEscapeString . T.unpack . href
 
-compareDate :: T.Text -> T.Text -> Ordering
-compareDate _ _ = EQ
+compareDate :: Entry -> Entry -> Ordering
+compareDate = comparing lastModified
+
+compareName :: Entry -> Entry -> Ordering
+compareName = comparing decodedName
+
 
