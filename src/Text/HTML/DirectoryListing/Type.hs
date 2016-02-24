0b54b401a8492ad94475aba8e24b4f5c63032a5c
mingchuan <ming@culpring.com>
2016-02-24 20:10:06 +0800

    update data type abstraction

--- src/Text/HTML/DirectoryListing/Type.hs
+++ src/Text/HTML/DirectoryListing/Type.hs
@@ -2,11 +2,15 @@ module Type
 where
 
 import qualified Data.Text as T
+import Data.Time.Format
+import Data.Time.LocalTime
 
 data Entry = Entry
     { name :: T.Text
     , href :: T.Text
-    , lastModified :: T.Text
-    , fileSize :: Maybe T.Text
+    , lastModified :: LocalTime
+    , fileSize :: Maybe Integer
+    -- ^ file size represented in bytes,
+    -- Nothing if this Entry is a folder
     }
 
